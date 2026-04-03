c2382468c62e40c8692ed78fe9b206e5a59e9857
dangLuan01 <luanboss012@gmail.com>
2026-04-03 08:48:53 +0700

    update favicon

--- components/layout/MobileHeader.tsx
+++ components/layout/MobileHeader.tsx
@@ -2,7 +2,7 @@
 const MobileHeader = () => {
   return (
     <div className="md:hidden flex justify-between items-center p-4 bg-white sticky top-0 z-50 border-b border-slate-100">
-        <span className="text-xl font-extrabold tracking-tighter">ThiThu<span className="text-indigo-600">.vn</span></span>
+        <span className="text-xl font-extrabold tracking-tighter">Toiecviet<span className="text-indigo-600">.vn</span></span>
         <div className="flex gap-4">
             <i className="fas fa-search text-slate-400 text-lg"></i>
             <i className="far fa-bell text-slate-400 text-lg"></i>
